d28061ffd1de71c4201379320f22fc446a45223a
Maik Pütz <maikpuetz@gmail.com>
2022-01-24 22:36:14 +0100

    Implemented first api endpoint documentation

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -9,8 +9,101 @@ use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
+/**
+ * @group Publisher management
+ */
 class PublisherController extends Controller
 {
+    /**
+     * Fetch a list of all publishers
+     *
+     * This endpoint will return a list of all publishers. You can sort the list by publisher name or founded year. You
+     * also have the ability to filter the list by publisher name, country, founded year of between two years.
+     *
+     *
+     * @authenticated
+     *
+     * @header Accept application/json
+     *
+     * @queryParam sort The field to sort the list by. Example: name
+     * @queryParam filter[name] string Filter by publisher name. Example: Marvel
+     * @queryParam filter[country] string Filter by publisher country. Example: USA
+     * @queryParam filter[founded-year] int Filter by publisher founded year. Example: 1940
+     * @queryParam filter[founded-year-between] string Filter by publisher founded year in the given range. Example: 1940,1990
+     * @queryParam page[number] int The page number (default 1). Example: 2
+     * @queryParam page[size] int The page size (default 30). Example: 10
+     *
+     * @response {
+     *   "data": [
+     *     {
+     *       "id": 1,
+     *       "name": "Publisher 1",
+     *       "founded-at": "1990",
+     *       "website-url": "http://www.publisher1.com",
+     *       "twitter-url": "http://www.twitter.com/publisher1",
+     *       "address": "Address 1",
+     *       "city": "City 1",
+     *       "state": "State 1",
+     *       "zip": "12345",
+     *       "country": "DEU",
+     *       "created-at": "2020-01-01T00:00:00+00:00",
+     *       "updated-at": "2020-01-01T00:00:00+00:00"
+     *     },
+     *     {
+     *       "id": 2,
+     *       "name": "Publisher 2",
+     *       "founded-at": "1990",
+     *       "website-url": "http://www.publisher2.com",
+     *       "twitter-url": "http://www.twitter.com/publisher2",
+     *       "address": "Address 2",
+     *       "city": "City 2",
+     *       "state": "State 2",
+     *       "zip": "12345",
+     *       "country": "DEU",
+     *       "created-at": "2020-01-01T00:00:00+00:00",
+     *       "updated-at": "2020-01-01T00:00:00+00:00"
+     *     },
+     *   ],
+     *   "links": {
+     *     "first": "http://localhost/api/publishers?page%5Bnumber%5D=1",
+     *     "last": "http://localhost/api/publishers?page%5Bnumber%5D=4",
+     *     "prev": null,
+     *     "next": "http://localhost/api/publishers?page%5Bnumber%5D=2"
+     *   },
+     *   "meta": {
+     *      "current_page": 1,
+     *      "from": 1,
+     *      "last_page": 4,
+     *      "links": [
+     *        {
+     *          "url": null,
+     *          "label": "&laquo; Previous",
+     *          "active": false
+     *        },
+     *        {
+     *          "url": "http://localhost/api/publishers?sort=name&page%5Bnumber%5D=1",
+     *          "label": "1",
+     *          "active": true
+     *        },
+     *        {
+     *          "url": "http://localhost/api/publishers?sort=name&page%5Bnumber%5D=2",
+     *          "label": "2",
+     *          "active": false
+     *        },
+     *        {
+     *          "url": "http://localhost/api/publishers?sort=name&page%5Bnumber%5D=2",
+     *          "label": "Next &raquo;",
+     *          "active": false
+     *        }
+     *      ],
+     *      "path": "http://localhost/api/publishers",
+     *      "per_page": 30,
+     *      "to": 30,
+     *      "total": 100
+     *    }
+     * }
+     *
+     */
     public function index(): AnonymousResourceCollection
     {
         $publishers = QueryBuilder::for(Publisher::class)
@@ -30,11 +123,72 @@ class PublisherController extends Controller
         return PublisherResource::collection($publishers);
     }
 
+    /**
+     * Fetch a single publisher
+     *
+     * This endpoint will return a single publisher specified by the id.
+     *
+     * @authenticated
+     *
+     * @response {
+     *   "data": {
+     *     "id": 1,
+     *     "name": "Publisher 1",
+     *     "founded-at": "1990",
+     *     "website-url": "http://www.publisher1.com",
+     *     "twitter-url": "http://www.twitter.com/publisher1",
+     *     "address": "Address 1",
+     *     "city": "City 1",
+     *     "state": "State 1",
+     *     "zip": "12345",
+     *     "country": "DEU",
+     *     "created-at": "2020-01-01T00:00:00+00:00",
+     *     "updated-at": "2020-01-01T00:00:00+00:00"
+     *   }
+     * }
+     */
     public function show(Publisher $publisher): PublisherResource
     {
         return new PublisherResource($publisher);
     }
 
+    /**
+     * Create a new publisher
+     *
+     * This endpoint will create a new publisher.
+     *
+     * @authenticated
+     *
+     * @header Accept application/json
+     *
+     * @bodyParam name string required The name of the publisher.
+     *
+     * @response {
+     *   "data": {
+     *     "id": 1,
+     *     "name": "Publisher 1",
+     *     "founded-at": "1990",
+     *     "website-url": "http://www.publisher1.com",
+     *     "twitter-url": "http://www.twitter.com/publisher1",
+     *     "address": "Address 1",
+     *     "city": "City 1",
+     *     "state": "State 1",
+     *     "zip": "12345",
+     *     "country": "DEU",
+     *     "created-at": "2020-01-01T00:00:00+00:00",
+     *     "updated-at": "2020-01-01T00:00:00+00:00"
+     *   }
+     * }
+     *
+     * @response 422 {
+     *     "message": "The given data was invalid.",
+     *     "errors": {
+     *         "field": [
+     *             "Error message"
+     *         ]
+     *     }
+     * }
+     */
     public function store(PublisherRequest $request): PublisherResource
     {
         $publisher = Publisher::create($request->validated());
@@ -42,6 +196,41 @@ class PublisherController extends Controller
         return new PublisherResource($publisher);
     }
 
+    /**
+     * Update an existing publisher
+     *
+     * This endpoint will update an existing publisher.
+     *
+     * @authenticated
+     *
+     * @header Accept application/json
+     *
+     * @response {
+     *   "data": {
+     *     "id": 1,
+     *     "name": "Publisher 1",
+     *     "founded-at": "1990",
+     *     "website-url": "http://www.publisher1.com",
+     *     "twitter-url": "http://www.twitter.com/publisher1",
+     *     "address": "Address 1",
+     *     "city": "City 1",
+     *     "state": "State 1",
+     *     "zip": "12345",
+     *     "country": "DEU",
+     *     "created-at": "2020-01-01T00:00:00+00:00",
+     *     "updated-at": "2020-01-01T00:00:00+00:00"
+     *   }
+     * }
+     *
+     * @response 422 {
+     *     "message": "The given data was invalid.",
+     *     "errors": {
+     *         "field": [
+     *             "Error message"
+     *         ]
+     *     }
+     * }
+     */
     public function update(PublisherRequest $request, Publisher $publisher): PublisherResource
     {
         $publisher->update($request->validated());
@@ -49,6 +238,44 @@ class PublisherController extends Controller
         return new PublisherResource($publisher);
     }
 
+    /**
+     * Delete an existing publisher
+     *
+     * This endpoint will delete an existing publisher.
+     *
+     * @authenticated
+     *
+     * @header Accept application/json
+     *
+     * @response {
+     *   "data": {
+     *     "id": 1,
+     *     "name": "Publisher 1",
+     *     "founded-at": "1990",
+     *     "website-url": "http://www.publisher1.com",
+     *     "twitter-url": "http://www.twitter.com/publisher1",
+     *     "address": "Address 1",
+     *     "city": "City 1",
+     *     "state": "State 1",
+     *     "zip": "12345",
+     *     "country": "DEU",
+     *     "created-at": "2020-01-01T00:00:00+00:00",
+     *     "updated-at": "2020-01-01T00:00:00+00:00"
+     *   }
+     * }
+     *
+     * @response 404 {
+     *  "message": "Resource not found."
+     * }
+     *
+     * @response 422 {
+     *   "message": "The given data was invalid.",
+     *   "errors": {
+     *   "name": [
+     *      "Error message"
+     *   ]
+     * }
+     */
     public function destroy(Publisher $publisher): PublisherResource
     {
         $publisher->delete();
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,8 +7,35 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 
+/**
+ * @group User management
+ * @header Accept application/json
+ */
 class RegisterController extends Controller
 {
+    /**
+     * Register a new user
+     *
+     * This endpoint allows you to register a new user. The email address must be unique or you will get a 422 with an
+     * error message that the email address has already been taken.
+     *
+     * @unauthenticated
+     *
+     * @header Accept application/json
+     *
+     * @response {
+     *     "access_token": "token",
+     *     "token_type": "Bearer",
+     * }
+     *
+     * @response 422 {
+     *   "message": "The given data was invalid.",
+     *   "errors": {
+     *   "name": [
+     *     "Error message."
+     *   ]
+     * }
+     */
     public function register(RegisterRequest $request): JsonResponse
     {
         $user = User::create([
