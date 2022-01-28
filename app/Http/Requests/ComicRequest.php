d3df653cf65dc1a31b23e8bca67adfbfae093506
Maik Pütz <maikpuetz@gmail.com>
2022-01-28 22:23:00 +0100

    Fixed unique bug for fetching current series

--- app/Http/Requests/ComicRequest.php
+++ app/Http/Requests/ComicRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Series;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -14,14 +15,23 @@ class ComicRequest extends FormRequest
 
     public function rules(): array
     {
+        /** @var Series $series */
+        $series = $this->route('series');
         $requiredRule = $this->method() === 'POST' ? 'required' : 'sometimes';
 
         return [
             'title' => ['sometimes', 'string', 'max:200'],
-            'issue_number' => [$requiredRule, 'integer', 'min:0', Rule::unique('comics')->where('series_id', $this->series_id)],
+            'issue_number' => [$requiredRule, 'integer', 'min:0', Rule::unique('comics', 'issue_number')->where('series_id', $series->id)],
             'description' => ['nullable', 'string', 'max:1000'],
             'format' => [$requiredRule, 'string', 'max:50'],
             'page_count' => ['nullable', 'integer', 'min:2', 'max:1000'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'issue_number.unique' => 'This issue number is already in use for this series.',
         ];
     }
 }
